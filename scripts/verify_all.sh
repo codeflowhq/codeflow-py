2f21c480cfbf302e9682471b1d8b81dfc4360d9d
yuchii <yinqitang17@gmail.com>
2026-05-12 10:39:01 +0800

    refactor

--- scripts/verify_all.sh
+++ scripts/verify_all.sh
@@ -6,8 +6,7 @@ WEB_DIR="/Users/bbnb/sg/code_visualizer_web/web_app"
 
 cd "$ROOT_DIR"
 uv run pytest tests -q
-uv run mypy src/code_visualizer/builders src/code_visualizer/views src/code_visualizer/tracing src/code_visualizer/graph_builder.py src/code_visualizer/graph_view_builder.py src/code_visualizer/browser_api.py
-uv run ruff check src/code_visualizer/builders src/code_visualizer/views src/code_visualizer/tracing src/code_visualizer/graph_builder.py src/code_visualizer/graph_view_builder.py src/code_visualizer/browser_api.py src/code_visualizer/view_utils.py tests
-python3 "$WEB_DIR/scripts/sync_python_runtime.py"
+uv run mypy src/code_visualizer
+uv run ruff check src/code_visualizer tests
 cd "$WEB_DIR"
 npm run build
